7f93e6bc0c72288af5a2f15b41b32e0c2112bb2b
JJ <jedi.dookun@outlook.com>
2018-05-15 15:47:31 -0700

    Added picture to contact page

---
+++ src/inc/contact/_contact.info.php
@@ -0,0 +1,24 @@
+<table>
+  <tr>
+    <td>
+      <img src="<?php bloginfo('template_url'); ?>/img/icons/email.png" alt="">
+    </td>
+    <td>jane@janedoepiano.com</td>
+  </tr>
+  <tr>
+    <td>
+      <img src="<?php bloginfo('template_url'); ?>/img/icons/phone.png" alt="">
+    </td>
+    <td>(818) 123-4567</td>
+  </tr>
+  <tr>
+    <td>
+      <img src="<?php bloginfo('template_url'); ?>/img/icons/home.png" alt="">
+    </td>
+    <td>12345 Street St.</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>Woodland Hills, CA 91364</td>
+  </tr>
+</table>
--- src/inc/contact/_contact.php
+++ src/inc/contact/_contact.php
@@ -5,30 +5,8 @@
   </div>
   <div class="home-contact--container contact--container">
     <div class="home-contact--container--info contact--container--info">
-      <table>
-        <tr>
-          <td>
-            <img src="<?php bloginfo('template_url'); ?>/img/icons/email.png" alt="">
-          </td>
-          <td>jane@janedoepiano.com</td>
-        </tr>
-        <tr>
-          <td>
-            <img src="<?php bloginfo('template_url'); ?>/img/icons/phone.png" alt="">
-          </td>
-          <td>(818) 123-4567</td>
-        </tr>
-        <tr>
-          <td>
-            <img src="<?php bloginfo('template_url'); ?>/img/icons/home.png" alt="">
-          </td>
-          <td>12345 Street St.</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td>Woodland Hills, CA 91364</td>
-        </tr>
-      </table>
+      <img src="<?php bloginfo('template_url'); ?>/img/girls-at-the-piano-1922.jpg" alt="">
+      <small>Zinaida Serebriakova - Girls at the piano (1922)</small>
     </div>
     <div class="container contact-form">
       <form class="contact-form--form">
